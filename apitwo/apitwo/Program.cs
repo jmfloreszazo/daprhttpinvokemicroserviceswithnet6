3d4b8a21900336695a3a1f872acdbaa2762f7487
Jose Maria Flores Zazo <jmfloreszazo@gmail.com>
2022-04-08 20:55:18 +0200

    Add dapr invoke

--- apitwo/apitwo/Program.cs
+++ apitwo/apitwo/Program.cs
@@ -1,3 +1,5 @@
+using Dapr.Client;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,27 +10,11 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/test", async () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var httpClient = DaprClient.CreateInvokeHttpClient("apione");
+    return await httpClient.GetAsync("weatherforecast");
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
 });
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
